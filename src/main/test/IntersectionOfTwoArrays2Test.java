205d1b56ef66251a9db38f9ee6e4563d88f30e88
calvinraveenthran <calvin.raveenthran@gmail.com>
2020-07-29 19:14:03 -0400

    IntersectionOfTwoArrays2

--- src/main/test/IntersectionOfTwoArrays2Test.java
+++ src/main/test/IntersectionOfTwoArrays2Test.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 public class IntersectionOfTwoArrays2Test {
 
