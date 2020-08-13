c995b5368f5a67c8d9739bdee1e55b73383802bc
calvinraveenthran <calvin.raveenthran@gmail.com>
2020-08-12 21:27:39 -0400

    Longest common prefix

--- src/main/java/LongestCommonPrefix.java
+++ src/main/java/LongestCommonPrefix.java
@@ -1,7 +1,5 @@
 package main.java;
 
-import java.util.HashMap;
-
 public class LongestCommonPrefix {
 
     public String longestCommonPrefix(String[] strs) {
@@ -9,7 +7,7 @@ public class LongestCommonPrefix {
         StringBuilder sb = new StringBuilder();
         if(strs.length == 0)
             return sb.toString();
-        
+
         for(int j = 0; j< strs[0].length(); j++) {
             char toMatch = strs[0].charAt(j);
             for (int i = 1; i < strs.length; i++) {
