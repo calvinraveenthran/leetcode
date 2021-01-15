292aa9d542da778f10ff4d60ff2c05a68ca31ba1
calvinraveenthran <calvin.raveenthran@gmail.com>
2021-01-15 01:50:28 -0500

    Min and Max heap

---
+++ src/main/java/MaxHeap.java
@@ -0,0 +1,75 @@
+package main.java;
+
+public class MaxHeap {
+
+    private int[] heap;
+    private int size;
+    private int maxsize;
+
+    public MaxHeap(int size) {
+        this.maxsize = size;
+        this.size =0;
+        this.heap = new int[this.maxsize +1];
+        heap[0] = Integer.MAX_VALUE;
+    }
+
+    private int parent(int pos) {
+        return pos/2;
+    }
+
+    private int leftChild(int pos) {
+        return (pos*2);
+    }
+
+    private int rightChild(int pos) {
+        return (pos*2) +1;
+    }
+
+    private void swap(int parent, int child) {
+        int tmp;
+        tmp = heap[parent];
+        heap[parent] = heap[child];
+        heap[child] = tmp;
+    }
+
+    private void downHeapify(int pos) {
+        if(pos > (size/2) && pos <= size){
+            return;
+        }
+
+        if(heap[pos] < heap[leftChild(pos)] || heap[pos] < heap[rightChild(pos)]) {
+
+            if(heap[leftChild(pos)] > heap[rightChild(pos)]){
+                swap(pos, leftChild(pos));
+                downHeapify(leftChild(pos));
+            } else{
+                swap(pos, rightChild(pos));
+                downHeapify(rightChild(pos));
+            }
+        }
+    }
+
+    private void heapifyUp(int pos){
+        int temp = heap[pos];
+        while(pos > 0 && temp > heap[parent(pos)]) {
+            heap[pos] = heap[parent(pos)];
+            pos = parent(pos);
+        }
+        heap[pos] = temp;
+    }
+
+    public void insert(int element){
+
+        size += 1;
+        heap[size] = element;
+        heapifyUp(size);
+    }
+
+    public int extractMax() {
+        int max = heap[1];
+        heap[1] = heap[size];
+        size = size-1;
+        downHeapify(1);
+        return max;
+    }
+}
---
+++ src/main/java/MinHeap.java
@@ -0,0 +1,75 @@
+package main.java;
+
+public class MinHeap {
+
+    private int[] heap;
+    private int size;
+    private int maxsize;
+
+    public MinHeap(int size) {
+        this.maxsize = size;
+        this.size =0;
+        this.heap = new int[this.maxsize +1];
+        heap[0] = Integer.MIN_VALUE;
+    }
+
+    private int parent(int pos) {
+        return pos/2;
+    }
+
+    private int leftChild(int pos) {
+        return (pos*2);
+    }
+
+    private int rightChild(int pos) {
+        return (pos*2) +1;
+    }
+
+    private void swap(int parent, int child) {
+        int tmp;
+        tmp = heap[parent];
+        heap[parent] = heap[child];
+        heap[child] = tmp;
+    }
+
+    private void downHeapify(int pos) {
+        if(pos > (size/2) && pos <= size){
+            return;
+        }
+
+        if(heap[pos] > heap[leftChild(pos)] || heap[pos] > heap[rightChild(pos)]) {
+
+            if(heap[leftChild(pos)] < heap[rightChild(pos)]){
+                swap(pos, leftChild(pos));
+                downHeapify(leftChild(pos));
+            } else{
+                swap(pos, rightChild(pos));
+                downHeapify(rightChild(pos));
+            }
+        }
+    }
+
+    private void heapifyUp(int pos){
+        int temp = heap[pos];
+        while(pos > 0 && temp < heap[parent(pos)]) {
+            heap[pos] = heap[parent(pos)];
+            pos = parent(pos);
+        }
+        heap[pos] = temp;
+    }
+
+    public void insert(int element){
+
+        size += 1;
+        heap[size] = element;
+        heapifyUp(size);
+    }
+
+    public int extractMin() {
+        int min = heap[1];
+        heap[1] = heap[size];
+        size = size-1;
+        downHeapify(1);
+        return min;
+    }
+}
